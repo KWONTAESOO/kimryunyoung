97096b565ea6b1c5776c64b2ffd06d5cfda2317a
KWONTAESOO <lv4ukts@nate.com>
2022-11-18 12:15:49 +0900

    Update script.js

--- resources/script.js
+++ resources/script.js
@@ -868,11 +868,7 @@ $(document).ready(function() {
 
 });
 
-function copy_to_clipboard() {    
-	  var copyText = document.getElementById('myInput');
-	  copyText.select();
-	  copyText.setSelectionRange(0, 99999);
-	  document.execCommand("Copy");
-	  alert('복사되었습니다, 감사합니다.');
-	}
-	 
+function copy_to_clipboard(str) {
+  window.clipboardData.setData("Text", str);
+  alert("복사되었습니다.");
+}
